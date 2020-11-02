3f5baf8ca797c43385a64cd1ec7ca9b83dab17a2
fedems <fedems1982@gmail.com>
2020-11-02 18:40:46 -0300

    first commit

--- box/box.js
+++ box/box.js
@@ -47,117 +47,103 @@
 			am4core.useTheme(am4themes_animated);
 			// Themes end
             
-            var chart = am4core.create(divid, am4charts.XYChart);
-            chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
-            
-            chart.data = [
-              {
-                country: "USA",
-                visits: 23725
-              },
-              {
-                country: "China",
-                visits: 1882
-              },
-              {
-                country: "Japan",
-                visits: 1809
-              },
-              {
-                country: "Germany",
-                visits: 1322
-              },
-              {
-                country: "UK",
-                visits: 1122
-              },
-              {
-                country: "France",
-                visits: 1114
-              },
-              {
-                country: "India",
-                visits: 984
-              },
-              {
-                country: "Spain",
-                visits: 711
-              },
-              {
-                country: "Netherlands",
-                visits: 665
-              },
-              {
-                country: "Russia",
-                visits: 580
-              },
-              {
-                country: "South Korea",
-                visits: 443
-              },
-              {
-                country: "Canada",
-                visits: 441
-              }
-            ];
-            
-            var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
-            categoryAxis.renderer.grid.template.location = 0;
+            // Create chart instance
+            var chart = am4core.create(divid, am4charts.XYChart3D);
+
+            // Add data
+            chart.data = [{
+              "country": "USA",
+              "visits": 4025
+            }, {
+              "country": "China",
+              "visits": 1882
+            }, {
+              "country": "Japan",
+              "visits": 1809
+            }, {
+              "country": "Germany",
+              "visits": 1322
+            }, {
+              "country": "UK",
+              "visits": 1122
+            }, {
+              "country": "France",
+              "visits": 1114
+            }, {
+              "country": "India",
+              "visits": 984
+            }, {
+              "country": "Spain",
+              "visits": 711
+            }, {
+              "country": "Netherlands",
+              "visits": 665
+            }, {
+              "country": "Russia",
+              "visits": 580
+            }, {
+              "country": "South Korea",
+              "visits": 443
+            }, {
+              "country": "Canada",
+              "visits": 441
+            }, {
+              "country": "Brazil",
+              "visits": 395
+            }, {
+              "country": "Italy",
+              "visits": 386
+            }, {
+              "country": "Australia",
+              "visits": 384
+            }, {
+              "country": "Taiwan",
+              "visits": 338
+            }, {
+              "country": "Poland",
+              "visits": 328
+            }];
+
+            // Create axes
+            let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
             categoryAxis.dataFields.category = "country";
-            categoryAxis.renderer.minGridDistance = 40;
-            categoryAxis.fontSize = 11;
+            categoryAxis.renderer.labels.template.rotation = 270;
+            categoryAxis.renderer.labels.template.hideOversized = false;
+            categoryAxis.renderer.minGridDistance = 20;
+            categoryAxis.renderer.labels.template.horizontalCenter = "right";
+            categoryAxis.renderer.labels.template.verticalCenter = "middle";
+            categoryAxis.tooltip.label.rotation = 270;
+            categoryAxis.tooltip.label.horizontalCenter = "right";
+            categoryAxis.tooltip.label.verticalCenter = "middle";
 
-            var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-            valueAxis.min = 0;
-            valueAxis.max = 24000;
-            valueAxis.strictMinMax = true;
-            valueAxis.renderer.minGridDistance = 30;
-            
-            // axis break
-            var axisBreak = valueAxis.axisBreaks.create();
-            axisBreak.startValue = 2100;
-            axisBreak.endValue = 22900;
-            //axisBreak.breakSize = 0.005;
+            let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+            valueAxis.title.text = "Countries";
+            valueAxis.title.fontWeight = "bold";
 
-            // fixed axis break
-            var d = (axisBreak.endValue - axisBreak.startValue) / (valueAxis.max - valueAxis.min);
-            axisBreak.breakSize = 0.05 * (1 - d) / d; // 0.05 means that the break will take 5% of the total value axis height
-
-            // make break expand on hover
-            var hoverState = axisBreak.states.create("hover");
-            hoverState.properties.breakSize = 1;
-            hoverState.properties.opacity = 0.1;
-            hoverState.transitionDuration = 1500;
-
-            axisBreak.defaultState.transitionDuration = 1000;
-            /*
-            // this is exactly the same, but with events
-            axisBreak.events.on("over", function() {
-              axisBreak.animate(
-                [{ property: "breakSize", to: 1 }, { property: "opacity", to: 0.1 }],
-                1500,
-                am4core.ease.sinOut
-              );
-            });
-            axisBreak.events.on("out", function() {
-              axisBreak.animate(
-                [{ property: "breakSize", to: 0.005 }, { property: "opacity", to: 1 }],
-                1000,
-                am4core.ease.quadOut
-              );
-            });*/
-
-            var series = chart.series.push(new am4charts.ColumnSeries());
-            series.dataFields.categoryX = "country";
+            // Create series
+            var series = chart.series.push(new am4charts.ColumnSeries3D());
             series.dataFields.valueY = "visits";
-            series.columns.template.tooltipText = "{valueY.value}";
-            series.columns.template.tooltipY = 0;
-            series.columns.template.strokeOpacity = 0;
+            series.dataFields.categoryX = "country";
+            series.name = "Visits";
+            series.tooltipText = "{categoryX}: [bold]{valueY}[/]";
+            series.columns.template.fillOpacity = .8;
 
-            // as by default columns of the same series are of the same color, we add adapter which takes colors from chart.colors color set
-            series.columns.template.adapter.add("fill", function(fill, target) {
+            var columnTemplate = series.columns.template;
+            columnTemplate.strokeWidth = 2;
+            columnTemplate.strokeOpacity = 1;
+            columnTemplate.stroke = am4core.color("#FFFFFF");
+
+            columnTemplate.adapter.add("fill", function(fill, target) {
               return chart.colors.getIndex(target.dataItem.index);
-            });
+            })
+
+            columnTemplate.adapter.add("stroke", function(stroke, target) {
+              return chart.colors.getIndex(target.dataItem.index);
+            })
+
+            chart.cursor = new am4charts.XYCursor();
+            chart.cursor.lineX.strokeOpacity = 0;
+            chart.cursor.lineY.strokeOpacity = 0;
             
 		  } 
           else {            	
